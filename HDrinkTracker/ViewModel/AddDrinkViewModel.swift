cf6851d06917c94cfa8300d2f3a2becf700bc78c
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2023-05-07 18:33:59 +0330

    Update AddDrinkViewModel and AddDrinkViewModelTests

--- HDrinkTracker/ViewModel/AddDrinkViewModel.swift
+++ HDrinkTracker/ViewModel/AddDrinkViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class AddDrinkViewModel {
+    @Published var name: String = ""
+    @Published var calories: String = ""
+    @Published var fat: String = ""
+    @Published var milliliters: String = ""
+    @Published var drinks: [Drink] = []
+    
+    func addDrink(name: String, calories: Int, fat: Int, milliliters: Int) {
+        if name.isEmpty || calories < 0 || fat < 0 || milliliters < 0 {
+            return
+        }
+        let drink = Drink(name: name, calories: calories, fat: fat, milliliters: milliliters)
+        drinks.append(drink)
+    }
+    
+    func clearFields() {
+        name = ""
+        calories = ""
+        fat = ""
+        milliliters = ""
+    }
+    
+}
--- HDrinkTrackerTests/ViewModelTests/AddDrinkViewModelTests.swift
+++ HDrinkTrackerTests/ViewModelTests/AddDrinkViewModelTests.swift
@@ -11,4 +11,56 @@ import XCTest
 
 class AddDrinkViewModelTests: XCTestCase {
     
+    var viewModel: AddDrinkViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = AddDrinkViewModel()
+    }
+    
+    func testAddDrink_AddsDrinkToDrinks() {
+        let initialCount = viewModel.drinks.count
+        viewModel.addDrink(name: "Coffee", calories: 50, fat: 1, milliliters: 240)
+        XCTAssertEqual(viewModel.drinks.count, initialCount + 1)
+        XCTAssertEqual(viewModel.drinks.last?.name, "Coffee")
+        XCTAssertEqual(viewModel.drinks.last?.calories, 50)
+        XCTAssertEqual(viewModel.drinks.last?.fat, 1)
+        XCTAssertEqual(viewModel.drinks.last?.milliliters, 240)
+    }
+    
+    func testAddDrink_WithInvalidName() {
+        let initialCount = viewModel.drinks.count
+        viewModel.addDrink(name: "", calories: 50, fat: 1, milliliters: 240)
+        XCTAssertEqual(viewModel.drinks.count, initialCount)
+    }
+    
+    func testAddDrink_WithInvalidCalories() {
+        let initialCount = viewModel.drinks.count
+        viewModel.addDrink(name: "Coffee", calories: -10, fat: 1, milliliters: 240)
+        XCTAssertEqual(viewModel.drinks.count, initialCount)
+    }
+    
+    func testAddDrink_WithNegativeFat() {
+        let initialCount = viewModel.drinks.count
+        viewModel.addDrink(name: "Coffee", calories: 50, fat: -1, milliliters: 240)
+        XCTAssertEqual(viewModel.drinks.count, initialCount)
+    }
+    
+    func testAddDrink_WithInvalidMilliliters() {
+        let initialCount = viewModel.drinks.count
+        viewModel.addDrink(name: "Coffee", calories: 50, fat: 1, milliliters: -240)
+        XCTAssertEqual(viewModel.drinks.count, initialCount)
+    }
+    
+    func testClearFields() {
+        viewModel.name = "Coffee"
+        viewModel.calories = "50"
+        viewModel.fat = "1"
+        viewModel.milliliters = "240"
+        viewModel.clearFields()
+        XCTAssertEqual(viewModel.name, "")
+        XCTAssertEqual(viewModel.calories, "")
+        XCTAssertEqual(viewModel.fat, "")
+        XCTAssertEqual(viewModel.milliliters, "")
+    }
 }
