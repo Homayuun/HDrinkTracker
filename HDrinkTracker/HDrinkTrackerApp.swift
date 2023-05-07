c61dac7950e8061b87114808ff0b59ed679014df
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2023-05-07 17:56:01 +0330

    Initial Commit

---
+++ HDrinkTracker/HDrinkTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDrinkTrackerApp.swift
+//  HDrinkTracker
+//
+//  Created by Homayun on 1402-02-17.
+//
+
+import SwiftUI
+
+@main
+struct HDrinkTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
